74615c4b43553f3796ce7a290c270e208fe59de5
Igor Gulamov <igor.gulamov@gmail.com>
2018-04-20 13:43:01 +0300

    add token properties

--- contracts/custom/VendingToken.sol
+++ contracts/custom/VendingToken.sol
@@ -3,6 +3,9 @@ import "../token/ERC20/ERC20DividendsToken.sol";
 
 contract VendingToken is ERC20DividendsToken {
   uint constant DECIMAL_MULTIPLIER = 10 ** 18;
+  string public name = "Vending Token";
+  string public symbol = "VEND";
+  uint8 public decimals = 18;
 
   function VendingToken() public {
     totalSupply_ = 100;
